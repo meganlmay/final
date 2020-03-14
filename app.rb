82468a84474898ebde21dd7a19cfb2ed9a5b9f9c
meganlmay <megan.may@kellogg.northwestern.edu>
2020-03-14 18:08:52 +0000

    final3

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@ require "sinatra/reloader" if development?                                      
 require "sequel"                                                                      #
 require "logger"                                                                      #
 require "twilio-ruby"                                                                 #
-require "bcrypt"                                                                      #
+require "bcrypt" 
+require "geocode"                                                                     #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
 DB ||= Sequel.connect(connection_string)                                              #
 DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
